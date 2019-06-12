38c1d31add589cd5b9d304f57170edea56eecf3b
John Fleming <jflemingnj@gmail.com>
2019-06-12 17:21:45 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,87 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Item extends Component {
+    render(){
+        let itemID = 'item' + this.props.itemID;
+        return (
+            <li id={this.props.item}><a href="#" onClick={this.props.action}>{this.props.item}</a></li>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class ShoppingList extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {listItems: []}
+
+        this.addItem = this.addItem.bind(this);
+        this.submit = this.submit.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    deleteItem(event){
+        event.preventDefault();
+        console.log(event.target.parentNode.id);
+        let aryListItems = this.state.listItems;
+        let aryLength = aryListItems.indexOf(event.target.parentNode.id);
+        console.log(aryListItems.indexOf(event.target.parentNode.id));
+        aryListItems.splice(aryLength, 1);
+        console.log(aryListItems.length);
+        this.setState({
+            listItems: aryListItems
+        }, () => {
+            console.log(this.state.listItems);
+          })        
+    }
+
+    addItem(event){
+        console.log(event.target.parentNode.getElementsByTagName('input')[0].value);
+        this.setState({
+            listItems: [...this.state.listItems, event.target.parentNode.getElementsByTagName('input')[0].value]
+        }, () => {
+            console.log(this.state.listItems);
+          })
+        /*this.setState((prevState) => {
+            let aryListItems = prevState.listItems;
+            aryListItems.push(event.target.parentNode.getElementsByTagName('input')[0].value);
+            return {listItems: aryListItems}
+        });*/
+    }
+
+    submit(event){
+        event.preventDefault();
+    }
+
+    render(){
+        return (
+            <div>
+                <section>
+                    <form onSubmit={this.submit}>
+                        <label>Add new item:</label>
+                        <input id="newListItem" type="text"></input>
+                        <button onClick={this.addItem}>Add</button>
+                    </form>
+                </section>
+                <section>
+                    <ul>
+                        {
+                            (this.state.listItems.length > 0) ? this.state.listItems.map((item, index)=>
+                                <Item key={index} item={item} itemID={index} action={this.deleteItem}/>
+                            ) : "Shopping cart is empty"
+                        }
+                    </ul>
+                </section>
+            </div>
+        )
+    }
+
+   /* static propTypes = {
+        listItems: PropTypes.array
+    }*/
+}
+
+ReactDOM.render(<ShoppingList />, document.getElementById('root'));
